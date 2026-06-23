4dd50cc5a5b71c3fd98224b4f7507044d48a7e49
anwesh2006 <anweshpatro9@gmail.com>
2026-06-23 21:59:16 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${mongoose.connection.host}`);
+    } catch (error) {
+        console.error('MongoDB connection error:', error.message);
+        process.exit(1);
+    }
+};
+
+// Auto-connect when this file is required
+connectDB();
+
+module.exports = connectDB;
