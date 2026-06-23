b1f34f99bdb2462c72ad93d349ad8bbc952ce5c0
Anwesh <anweshpatro9@gmail.com>
2026-06-24 01:15:03 +0530

    Fix: Trust proxy for secure session cookies on Vercel

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ require('dotenv').config();
 
 const app = express();
 
+// Trust proxy for secure cookies in production (Vercel)
+app.set('trust proxy', 1);
+
 // Connect to MongoDB
 require('./config/db');
 
